0e30640fb3d36c45425351e9e9be8e1e423e6a8e
Jared Grove <jaredkgrove@gmail.com>
2019-02-24 00:40:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(num)
   if num % 3 == 0 && numb % 5 == 0
     "FizzBuzz"
-    elsif num % 3
+    elsif num % 3 == 0
     "Fizz"
-  elsif num % 5
+  elsif num % 5 == 0
   "Buzz"
 else
   num
